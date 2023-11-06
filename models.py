9dc57323d8950cac060110f238b8ede56ce5e038
Anna Veber <53593640+AnnaIvlev@users.noreply.github.com>
2023-11-05 22:41:34 -0500

    Add files via upload

---
+++ models.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('posts', lazy=True))
+    image_url = db.Column(db.String(255), nullable=False)
+    text_content = db.Column(db.String(255))
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+class Like(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('likes', lazy=True))
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+    post = db.relationship('Post', backref=db.backref('likes', lazy=True))
